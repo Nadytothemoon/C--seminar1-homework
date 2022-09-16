3a24e8911e92e5ea6f90d5630d474cd1e2094d59
Nady <nadyyakunina@gmail.com>
2022-09-16 22:57:50 +0400

    4 tasks

---
+++ homework/Program.cs
@@ -0,0 +1,72 @@
+﻿// System.Console.Write("Введите число а:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число b:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a>b){
+//     System.Console.WriteLine("Число a> числа b");
+// }
+// else {
+//     if (b>a){
+//         System.Console.WriteLine("Число b> числа a");
+//     }
+//     else {
+//        System.Console.WriteLine("Числа равны");
+//     }
+// }
+
+
+
+// System.Console.Write("Введите число а:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число b:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число c:");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max=a;
+
+// if (max<b){
+//     max=b;
+// }
+// if (max<c){
+//     max=c;
+// }
+
+// System.Console.WriteLine(max);
+
+
+
+// System.Console.Write("Введите число а:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if(a%2==0){
+//     System.Console.WriteLine("Четное");
+// }
+// else {
+//     System.Console.WriteLine("Нечетное");
+// }
+
+System.Console.Write("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int i=1;
+
+while(i<=n){
+    if(i%2==0) {
+        System.Console.Write(i+" ");
+    }
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
